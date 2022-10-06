c81d7bf5efd6e52ca246d4705da4fa0ac2ea5dc6
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2022-10-06 13:13:43 +0530

    Activity 1

---
+++ Activity 1st.py
@@ -0,0 +1,36 @@
+# Built-in functions
+
+# abs() function
+x=abs(-31.2)
+print(x)
+
+# len() function
+x=len("HelloUjjwal")
+print(x)
+
+# min() function
+x=min(12,3,4,43,1)
+print(x)
+
+# max() function
+x=max(12,3,4,43,1)
+print(x)
+
+# round() function
+x=round(382.218223, 2)
+print(x)
+
+# isalnum() function
+txt="Ujjwal25"
+x=txt.isalnum()
+print(x)
+
+# type() function
+a=10
+print(a, "is of type", type(a))
+
+b=10.0
+print(b, "is of type", type(b))
+
+c=1 +2j
+print(c, "is of type", type(c))
