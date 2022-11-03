e30efb9f1eedb8a4d9596212fddfbf5d32d33af9
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2022-11-03 13:55:13 +0530

    Activity 7

---
+++ Activity_7.py
@@ -0,0 +1,17 @@
+a = input("What you want to Convert (C/F) : ")
+if (a == "C") or (a == "c"):
+    C = int(input("Enter the Temperature in Celsius : "))
+
+    F = (C * (9/5)) + 32 
+
+    print("Temperature in Farenheit is : %.2f F" %(F))
+    
+elif (a == "F") or (a == "f"):
+    F = int(input("Enter the Temperature in Farenheit : "))
+
+    C = (F - 32) * (5/9) 
+
+    print("Temperature in Farenheit is : %.2f C" %(C))
+    
+else:
+    print("Please Enter the Valid Input...!!!")
