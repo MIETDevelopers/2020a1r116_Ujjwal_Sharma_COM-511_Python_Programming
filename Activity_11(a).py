cfc301cd0b7c97b0529f7b6e406da9982bab07da
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2022-11-29 19:11:37 +0530

    Activity 11(a)

---
+++ Activity_11(a).py
@@ -0,0 +1,25 @@
+class Dog:
+    # Class Variable
+    animal = 'Dog'
+    # The init method or constructor
+    def __init__(self, breed, color):
+        # Instance Variable
+        self.breed = breed
+        self.color = color
+
+# Objects of Dog class
+Rodger = Dog("Pug", "Brown")
+Buzo = Dog("Bulldog", "Black")
+print('Rodger details:')
+print('Rodger is a', Rodger.animal)
+print('Breed :', Rodger.breed)
+print('Color :', Rodger.color)
+print('\nBuzo details:')
+print('Buzo is a', Buzo.animal)
+print('Breed :', Buzo.breed)
+print('Color :', Buzo.color)
+# Class variables can be accessed using class
+# name also
+print("\nAccessing class variable using class name")
+
+print(Dog.animal)
