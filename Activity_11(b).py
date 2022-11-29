33490dcadea8ac72846d3e1d018f249f02553623
2020A1R116 <76197681+2020A1R116@users.noreply.github.com>
2022-11-29 19:12:01 +0530

    Activity 11(b)

---
+++ Activity_11(b).py
@@ -0,0 +1,18 @@
+class Dog:
+# Class Variable
+    animal = 'Dog'
+    # The init method or constructor
+    def __init__(self, breed):
+        # Instance Variable
+        self.breed = breed
+    # Adds an instance variable
+    def setColor(self, color):
+        self.color = color
+    # Retrieves instance variable
+    def getColor(self):
+        return self.color
+
+# Driver Code
+Rodger = Dog("Pug")
+Rodger.setColor("Brown")
+print(Rodger.getColor())
